f23c270ef4864fbdc2d269cbffe3f8789db32077
giangtong-katalon <giang.tong@katalon.com>
2022-11-20 13:42:47 +0700

    Support Kraft

--- create-topics.sh
+++ create-topics.sh
@@ -4,6 +4,11 @@ if [[ -z "$KAFKA_CREATE_TOPICS" ]]; then
     exit 0
 fi
 
+if [[ -z "$KAFKA_PORT" ]]; then
+    echo "WARNING: missing dependent config: KAFKA_PORT. No topic will be created."
+    exit 0
+fi
+
 if [[ -z "$START_TIMEOUT" ]]; then
     START_TIMEOUT=600
 fi
@@ -45,13 +50,14 @@ IFS="${KAFKA_CREATE_TOPICS_SEPARATOR-,}"; for topicToCreate in $KAFKA_CREATE_TOP
 
     COMMAND="JMX_PORT='' ${KAFKA_HOME}/bin/kafka-topics.sh \\
 		--create \\
-		--zookeeper ${KAFKA_ZOOKEEPER_CONNECT} \\
+        --bootstrap-server localhost:${KAFKA_PORT} \\
 		--topic ${topicConfig[0]} \\
 		--partitions ${topicConfig[1]} \\
 		--replication-factor ${topicConfig[2]} \\
 		${config} \\
 		${KAFKA_0_10_OPTS} &"
     eval "${COMMAND}"
+    sleep 0.3
 done
 
 wait
